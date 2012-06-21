458e90500f4b923735ed914b0bbe6f5e9f51a43e
Vishrut Patel <vrp101@gmail.com>
2012-06-21 13:59:46 +0530

    Adding navigation bar

--- app/view/CalendarDisplay.js
+++ app/view/CalendarDisplay.js
@@ -1,12 +1,63 @@
 Ext.define('Med-Table.view.CalendarDisplay',{
         extend:'Ext.Panel',
         xtype:'calendardisplay',
-        html: '<embed src="calendar.html" width="100%" height="100%"/>',
 
         config:
         {
             title:'Calendar',
-            iconCls:'time'
+            iconCls:'time',
+            items:[
+                {   xtype: 'toolbar',
+                    ui:'light',
+                    height : 'auto',
+                    items: [
+                        {
+                            xtype: 'button',
+                            text : '<img src="resources/images/home-small.png">',
+                            handler: function()
+                            {
+                                Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Home'))
+                            },
+                            padding:'5px'
+                        },
+                        {
+                            xtype: 'spacer'
+                        },
+                        {
+                            xtype: 'button',
+                            text : '<img src="resources/images/reminder-small.png">',
+                            handler: function()
+                            {
+                                Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.CalendarDisplay'))
+                            },
+                            padding:'5px'
+                        },
+                        {
+                            xtype: 'button',
+                            text : '<img src="resources/images/instructions-small.png">',
+                            handler: function()
+                            {
+                                Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Menu'))
+                            },
+                            padding:'5px'
+                        },
+                        {
+                            xtype: 'button',
+                            text : '<img src="resources/images/appoint-small.png">',
+                            handler: function()
+                            {
+                                Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Menu'))
+                            },
+                            padding:'5px'
+                        }
+
+                    ]
+
+                },
+                {
+                    html: '<embed src="calendar.html" width="100%" height="100%"/>'
+                }
+            ]
         }
     }
 )
--- app/view/Menu.js
+++ app/view/Menu.js
@@ -4,27 +4,99 @@ Ext.define('Med-Table.view.Menu', {
     config:{
          title:'Menu',
          iconCls:'home',
+		
+		items: [
+		{   xtype: 'toolbar',
+            ui:'light',
+                height : 'auto',
+                items: [
+                    {
+                        xtype: 'button',
+                        text : '<img src="resources/images/home-small.png">',
+                        handler: function()
+                        {
+                            Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Home'))
+                        },
+                        padding:'5px'
+                    },
+                    {
+                        xtype: 'spacer'
+                    },
+                    {
+                        xtype: 'button',
+                        text : '<img src="resources/images/reminder-small.png">',
+                        handler: function()
+                        {
+                            Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Schedule'))
+                        },
+                        padding:'5px'
+                    },
+                    {
+                        xtype: 'button',
+                        text : '<img src="resources/images/instructions-small.png">',
+                        handler: function()
+                        {
+                            Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Menu'))
+                        },
+                        padding:'5px'
+                    },
+                    {
+                        xtype: 'button',
+                        text : '<img src="resources/images/appoint-small.png">',
+                        handler: function()
+                        {
+                            Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Menu'))
+                        },
+                        padding:'5px'
+                    }
 
+                ]
+
+            },
+		
+		{
          xtype: 'container',
          items:[
+             {  xtype: 'button',
+                 //cls: 'instructionImageButton',
+                 text : '<img src="resources/images/medicine reminder_without drop shadow.png">',
+                 handler: function()
+                 {
+                     Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.CalendarDisplay'))
+                 },
+
+                 left: '50px',
+                 top: '50px'
+             },
+
          {
             xtype: 'button',
-            cls: 'nextAppointmentButton',
-			left: '50px',
-			top: '50px',
-         },
-         {
-            xtype: 'button',
-            cls: 'medicineReminderButton',
+             text : '<img src="resources/images/instructions-01.png">',
+             handler: function()
+             {
+                 Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Schedule'))
+             },
+
+             //cls: 'medicineReminderButton',
             left: '387px',
 			top: '50px'
          },
-         {  xtype: 'button',
-            cls: 'instructionImageButton',
-            left: '714px',
-			top: '50px'
-         }
-         ]
+             {
+                 xtype: 'button',
+                 text : '<img src="resources/images/next appointment-01.png">',
+                 handler: function()
+                 {
+                     Ext.Viewport.setActiveItem(Ext.create('Med-Table.view.Home'))
+                 },
+                 //cls: 'nextAppointmentButton',
+                 left: '714px',
+                 top: '50px'
+
+             }
+
+        ]
+	 }
+	]
 
     }
 });
